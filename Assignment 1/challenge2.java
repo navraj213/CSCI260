6722bfff2042da3f314c9d49a78a85e2c2f9494c
Navraj Singh <navraj213@gmail.com>
2023-09-19 19:33:35 -0400

    intial commit

---
+++ Assignment 1/challenge2.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class challenge2 {
+
+    // Create a JAVA program that collects string data from a user’s input, adds each item to an Array List and then
+    // displays/outputs the contents of the Array List. You may either create a loop that continuously asks the users for data, or
+    // you may hard code 3 or more input requests. The display format is flexible and up to you how you want to display the
+    // values on screen.
+
+    
+}
