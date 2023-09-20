f7172bafe6bdd490a5fa4383ca066cf5872dff00
Navraj Singh <navraj213@gmail.com>
2023-09-19 20:00:47 -0400

    final changes

--- Assignment 1/challenge1.java
+++ Assignment 1/challenge1.java
@@ -1,10 +1,4 @@
 public class challenge1 {
-    // Programming Challenge 1:
-    // Create a JAVA program that reads in 10 unique values from an array and then displays each number next to its natural
-    // logarithm. The display format is flexible and up to you how you want to display the values on screen.
-    // Java Hint: “Math”
-    // Note: Each student must submit their solution to Canvas for the assignment to count
-
     public static void findLog(int x) {
         System.out.println("Number: " + x + " | Natural Log: " + Math.log(x));
     }
