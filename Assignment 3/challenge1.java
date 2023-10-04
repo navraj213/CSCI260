ba37919f6d463adbf20e9741d0ae09fe868d94a7
Risham Singh <risham316@gmail.com>
2023-10-03 20:00:17 -0400

    Finished

--- Assignment 3/challenge1.java
+++ Assignment 3/challenge1.java
@@ -8,21 +8,41 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.*;
 
 public class challenge1 {
+    private static void bubbleSort(ArrayList<Integer> intArray) {
+        Integer temp = 0;
+        for (int i = 0; i < intArray.size(); i++) {
+            for (int j = 1; j < (intArray.size() - i; j++))
+            {
+                if (intArray.get(j - 1) > intArray.get(j))
+                {
+                    temp = intArray.get(j - 1);
+                    intArray.set(j-1, intArray.get(j));
+                    intArray.set(j, temp);
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
-        ArrayList<Integer> nums = new ArrayList<integers>();
+        ArrayList<Integer> nums = new ArrayList<integer>();
         Scanner input = new Scanner(System.in);
         while(true)
         {
             System.out.println("Enter your integer (Type q to quit)");
             String keyboard = input.next();
-            try(Integer.parseIntegr(keyboard))
+            try(Integer.parseInteger(keyboard))
             {
                 nums.add(Integer.parseInteger(keyboard));
             }
-            catch(NumberFormatException)
+            catch(NumberFormatException e)
             {
                 break;
             }
         }
+        System.out.println();
+        System.out.println();
+        System.out.println("Current Array: " + nums);
+        bubbleSort(nums);
+        System.out.println("Sorted Array: " + nums);
+
     }
 }
