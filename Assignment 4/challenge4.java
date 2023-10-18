bbbdb5a5ec7cb82521e483069bc99cbc9ce4d7f9
Navraj Singh <nsingh59@nyit.edu>
2023-10-17 20:11:53 -0400

    finish challenge 4

--- Assignment 4/challenge4.java
+++ Assignment 4/challenge4.java
@@ -1,32 +1,43 @@
+import java.util.*;
 public class challenge4 {
-/*
- * Programming Challenge 4:
-You have just started work at your first programming job and you have been offered a salary
-plus benefits as part of your employment package. One of your benefits is the option to
-contribute to a 401k or a 403b plan to save for retirement. Write a program using a Stack(s)
-data structure that......
-a. Captures the amount you wish to contribute each year
-b. The percentage of interest that you think you will earn each year on average
-c. A display of the earnings you will earn each year over a 30 year period from the most
-recent year back to the first year
-Pseudocode:
-1. Capture the numerical amount to contribute (e.g. 19000)
-2. Store to a variable (A)
-3. Capture the interest rate you want to earn (B)
-4. Establish a variable to calculate/store the annual total (C)
-5. Establish a variable to store running total (D)
-6. Create an empty Stack data structure (TheStack)
-7. Create a loop that will loop through 30 cycles for each year
-8. Calculate the value of A * B and set it equal to C
-9. Store C to the Stack
-10. Add C to D
-11. Loop until all 30 values are stored
-12. Display Stack values
-13. Display Total Value D
- */
-
-
     public static void main(String[] args) {
-        
+        /*
+         * You have just started work at your first programming job and you have been offered a salary
+            plus benefits as part of your employment package. One of your benefits is the option to
+            contribute to a 401k or a 403b plan to save for retirement. Write a program using a Stack(s)
+            data structure that......
+
+            a. Captures the amount you wish to contribute each year
+            b. The percentage of interest that you think you will earn each year on average
+            c. A display of the earnings you will earn each year over a 30 year period from the most
+            recent year back to the first year
+         */
+
+         //a
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the amount you wish to contribute each year: ");
+        double contribution = input.nextDouble();
+
+        //b
+        System.out.println("Enter the percentage of interest that you think you will earn each year on average: ");
+        double interest = input.nextDouble();
+
+        //c
+        Stack<Double> earnings = new Stack<Double>();
+        double total = 0;
+        for(int i = 0; i < 30; i++) {
+            total += contribution;
+            earnings.push(total);
+            total += total * interest;
+        }
+
+        System.out.println("Earnings: ");
+        for(int i = 0; i < 30; i++) {
+            System.out.println(earnings.pop());
+        }
+
+        System.out.println("Total Balance: " + total);
+
+
     }
 }
