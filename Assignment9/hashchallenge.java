2c198b8d6c891ccf8fd52f233f25c7434a62b0b1
Navraj Singh <nsingh59@nyit.edu>
2023-11-14 18:22:36 -0500

    Create challenge 9

---
+++ Assignment9/hashchallenge.java
@@ -0,0 +1,19 @@
+package Assignment9;
+
+/*
+ * a. Use the sample code for the hash table below and confirm functionality on your system. This
+      code is also in Canvas under handouts-Hash Sample Code
+
+ * b. Create a program that captures boat names and boat hull numbers (e.g. 2364ZR) at the marina
+      from the system user, adds them to a hash table with appropriate keys and then displays the
+      values of the hash table when prompted by the user.
+
+ * c. Add a feature in your program from step b for a user to search for a value in the hash table
+   using its key and then display the result of the value to the user. Do NOT worry about error
+   handling for null values etc. for this exercise.
+ */
+
+
+public class hashchallenge {
+    
+}
